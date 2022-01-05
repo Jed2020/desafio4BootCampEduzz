4998e2d1cf6b655dc81b42e99d47e694f5d8ab7e
emerson <69053988+Jed2020@users.noreply.github.com>
2022-01-05 12:59:50 -0300

    create shorturl in controller and shorturl in model

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import config from 'config';
+import bodyParser from "body-parser";
 import routes from "./routes";
+import db from "./db";
 
 const app = express();
 
 const port = config.get("port");
 
+// parse application/json
+app.use(bodyParser.json());
+
 app.listen(4000, () => {
     console.log(`Application listening at http://localhost:${port}`);
+    db();
     routes(app);
+    
 });
