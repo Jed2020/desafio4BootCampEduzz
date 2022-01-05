d21ca233a3062abe28c18f52eaff9b62e31a1128
emerson <69053988+Jed2020@users.noreply.github.com>
2022-01-05 14:15:31 -0300

    using yup in middleware validateResourse

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,15 @@ import {
     createShortUrl,
     handleRedirect,
   } from "../controller/shortUrl.controller";
+import validateResourse from "../middleware/validateResourse";
+import shortUrlSchema from "../schemas/createShortUrl.schema";
 
 function routes(app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) => {
       return res.send("App is healthy");
     });
-    app.post("/api/url", createShortUrl);
+
+    app.post("/api/url", validateResourse(shortUrlSchema), createShortUrl);
 
     app.get("/:shortId", handleRedirect);
 }
